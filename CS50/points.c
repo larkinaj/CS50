c443e5565796987ac2badd4b1bfd91cc5353f2bf
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-05-22 19:22:31 +0000

    Sun, May 22, 2022, 7:22 PM +00:00

--- CS50/points.c
+++ CS50/points.c
@@ -3,11 +3,18 @@
 
 int main (void)
 {
-    int points = get_int("How many points did you lose? ")
+    int points = get_int("How many points did you lose? ");
 
     if (points < 2)
     {
-        printf("You lost fewer points as me.\n")
+        printf("You lost fewer points than me.\n");
     }
-    else if (points >2)
+    else if (points > 2)
+    {
+        printf("You lost more points than me.\n");
+    }
+    else
+    {
+        printf("You lost the same number as me.\n");
+    }
 }
