e398e56f36cf210568abf2928c98c8b994320cf5
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-05-24 21:05:22 +0000

    Tue, May 24, 2022, 9:05 PM +00:00

--- Week 0/calculator.c
+++ Week 0/calculator.c
@@ -6,15 +6,16 @@ int main(void)
     int x = get_int("x: ");
     int y = get_int("y: ");
 
-if (x<y)
-{
-    printf("%i is less than %i\n",x,y);
-}
-else if (x>y)
-{
-    printf("%i is greater than %i\n",x,y);
-}
-else
-{   printf("%i is equal to %i\n",x,y);
-}
+    if (x<y)
+    {
+        printf("%i is less than %i\n",x,y);
+    }
+    else if (x>y)
+    {
+        printf("%i is greater than %i\n",x,y);
+    }
+    else
+    {
+        printf("%i is equal to %i\n",x,y);
+    }
 }
