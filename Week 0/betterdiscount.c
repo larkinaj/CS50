9a6b70991aa9b8d74b443fa4ab6ed48f6d9259cd
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-05-24 00:47:04 +0000

    Tue, May 24, 2022, 12:47 AM +00:00

--- Week 0/betterdiscount.c
+++ Week 0/betterdiscount.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <cs50.h>
 
+
+
 int main (void)
 {
     float regular = get_float("Regular Price: ");
     float sale = regular * .85;
     printf("Sale Price: %.2f\n", sale);
+}
+
+
+
+float discount(float price)
+{
+    
+    return price * .85;
 }
