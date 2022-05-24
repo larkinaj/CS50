49704dafc2b53080238cd5a34c31bf4d716f1129
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-05-24 00:24:27 +0000

    Tue, May 24, 2022, 12:24 AM +00:00

--- Week 0/bettermeow.c
+++ Week 0/bettermeow.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
-int meow(int n);
+void meow(int n);
 
 int main(void)
 {
     meow(3);
 }
 
-int meow(int n)
+void meow(int n)
 {
     for (int i = 0; i < n; i++)
     {
-        return i;
         printf("meow\n");
     }
 }
