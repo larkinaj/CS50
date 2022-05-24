e1896a208f0fa1246bb7f9584bc9874900f03428
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-05-24 21:19:28 +0000

    Tue, May 24, 2022, 9:19 PM +00:00

--- Week 0/mario.c
+++ Week 0/mario.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
-
-int main (void)
+int main(void)
 {
-    for (int i = 0; i < 4; i++)
+    int n;
+    do
+    {
+        n = get_int("Width: ");
+    }
+    while (n<1);
+
+    for (int i=0; i<n; i++)
     {
         printf("?");
     }
