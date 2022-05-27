6233d0f29d575176b3cf539204b8ee4c40d67cb6
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-05-27 01:59:19 +0000

    Fri, May 27, 2022, 1:59 AM +00:00

--- Week 0/calculator.c
+++ Week 0/calculator.c
@@ -5,17 +5,18 @@ int main(void)
 {
     int x = get_int("x: ");
     int y = get_int("y: ");
+    char operator = get_char("Operator: ");
 
-    if (x<y)
+    if (char == "+")
     {
         printf("%i is less than %i\n",x,y);
     }
-    else if (x>y)
+    else if (char == "-")
     {
         printf("%i is greater than %i\n",x,y);
     }
     else
     {
-        printf("%i is equal to %i\n",x,y);
+        printf("Please pick plus or minus");
     }
 }
