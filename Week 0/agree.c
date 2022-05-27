2ba4cf686894d69dd8ac685f2ef77f41633100fa
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-05-27 23:57:57 +0000

    Fri, May 27, 2022, 11:57 PM +00:00

--- Week 0/agree.c
+++ Week 0/agree.c
@@ -4,11 +4,11 @@
 int main (void)
 {
     string agree = get_string("Do you agree? (yes/no) ");
-    if (agree == "yes" || agree == "Yes")
+    if (agree == "yes" || agree = "Yes")
     {
         printf("You have agreed.\n");
     }
-    else if (agree == "n" || agree == "No")
+    else if (agree = "n" || agree = "No")
     {
         printf("You have not agreed.\n");
     }
