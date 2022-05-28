148d9ccfdf92e3102671c5dd1d3eaf3a2d34e6b7
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-05-28 04:35:12 +0000

    Sat, May 28, 2022, 4:35 AM +00:00

---
+++ mario-more/mario2.c
@@ -0,0 +1,34 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+
+    do
+    {
+        // Get height
+        n = get_int("How tall should the pyramids be? (between 1 and 8): ");
+    }
+    // Keep asking for height until an input between 1-8 is given
+    while (n > 8 || n < 1);
+
+    // Pyramid row counter
+    for (int i = 0; i < n; i++)
+    {
+
+        // Space counter
+        for (int j = n - 1; j > i; j--)
+        {
+            printf(" ");
+        }
+
+        // Hash counter
+        for (int k = 0; k <= i; k++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+    }
+}
