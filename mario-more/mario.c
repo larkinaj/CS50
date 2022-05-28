7891ea481f155f48ce5b06e97ed48f17c700433e
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-05-28 01:08:59 +0000

    Sat, May 28, 2022, 1:08 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -16,7 +16,7 @@ int main(void)
     {
         for (int j = 1; j <= n; j++)
         {
-            blocks = j;
+            blocks = '#' + j;
             printf("%c", blocks);
         }
         printf("\n");
