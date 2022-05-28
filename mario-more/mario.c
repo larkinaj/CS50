178d66781d098947ddc2bd11de6fc4147f5bc697
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-05-28 00:38:41 +0000

    Sat, May 28, 2022, 12:38 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -3,8 +3,8 @@
 
 int main(void)
 {
-    
-    do int n = get_int("How tall should the pyramids be?(1-8) ")
+    int n = get_int("How tall should the pyramids be?(1-8) ")
+    do
 
 
     while n > 8 || n < 1
