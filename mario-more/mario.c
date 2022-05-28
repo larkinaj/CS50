cedc825dd79e59b6065ea962e9d6670da62214dd
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-05-28 04:26:42 +0000

    Sat, May 28, 2022, 4:26 AM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -7,15 +7,18 @@ int main(void)
 
     do
     {
+        // Get height
         n = get_int("How tall should the pyramids be? (between 1 and 8): ");
     }
+    // Keep asking for height until an input between 1-8 is given
     while (n > 8 || n < 1);
 
+    // Pyramid ow counter
     for (int i = 0; i < n; i++)
     {
         for (int j = n - 1; j > i; j--)
         {
-            printf(".");
+            printf(" ");
         }
         for (int k = 0; k <= i; k++)
         {
