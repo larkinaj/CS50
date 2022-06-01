b9478f8f54822dfe6cfe742ea5032a597fe93d93
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-06-01 03:48:24 +0000

    Wed, Jun 1, 2022, 3:48 AM +00:00

--- Week 0/inputdiscount.c
+++ Week 0/inputdiscount.c
@@ -6,8 +6,11 @@ float discount(float price, int percent_off2);
 int main (void)
 {
     float regular = get_float("Regular Price: ");
+
     int percent_off = get_int("Percent Off: ");
+
     float sale = discount(regular, percent_off);
+
     printf("Sale Price: %.2f\n", sale);
 }
 
