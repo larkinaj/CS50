f763f6d4274358e49fa0b9abc0ffbc26bdcac051
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-06-01 07:15:41 +0000

    Wed, Jun 1, 2022, 7:15 AM +00:00

---
+++ credit/test2.c
@@ -0,0 +1,14 @@
+#include <cs50.h>
+#include <stdio.h>
+
+long get_cardinput(void);
+bool get_amex(long cardinput);
+int get_length(long cardinput);
+
+int main(void)
+{
+    long cardinput = get_long("Number? ");
+
+    long mod = cardinput % 10000000000000 / 10000000000000;
+    printf("Mod: %li\n", mod);
+}
