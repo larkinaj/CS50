835e18d82ecec99d7fc7eb09d357eae6672d5764
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-06-01 05:32:18 +0000

    Wed, Jun 1, 2022, 5:32 AM +00:00

--- credit/test.c
+++ credit/test.c
@@ -4,7 +4,7 @@
 
 int main(void)
 {
-    long input = get_long("Number? ");
-    long mod = input % 100;
-    printf("Mod: %li\n", mod);
+    int input = get_long("Number? ");
+    int mod = input % 10;
+    printf("Mod: %i\n", mod);
 }
