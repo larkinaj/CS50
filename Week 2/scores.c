444d7b8499620dbabef00b42cc98f4478c9ff74d
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-06-02 23:53:05 +0000

    Thu, Jun 2, 2022, 11:53 PM +00:00

--- Week 2/scores.c
+++ Week 2/scores.c
@@ -6,9 +6,10 @@ int main(void)
 {
     int scores[3];
 
-    scores[0] = get_int("Score 1: ");
-    scores[1] = get_int("Score 2: ");
-    scores[2] = get_int("Score 3: ");
+    for (int i = 0; i < 3; i++)
+    {
+        scores[i] = get_int("Score: ");
+    }
 
     printf("Average: %.2f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
 }
