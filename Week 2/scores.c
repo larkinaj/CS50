02456ff9a1cabd05df44d47655b724abc3e108c3
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-06-02 23:47:39 +0000

    Thu, Jun 2, 2022, 11:47 PM +00:00

--- Week 2/scores.c
+++ Week 2/scores.c
@@ -4,9 +4,11 @@
 
 int main(void)
 {
-    int score1 = 72;
-    int score2 = 73;
-    int score3 = 33;
+    int scores[3];
+
+    score[0] = 72;
+    score[1] = 73;
+    score[2] = 33;
 
     printf("Average: %.2f\n", (score1 + score2 + score3) / 3.0);
 }
