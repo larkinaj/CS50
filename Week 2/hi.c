0efb27aa80573b822e6ceec5b940e2724c33e561
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-06-03 00:21:12 +0000

    Fri, Jun 3, 2022, 12:21 AM +00:00

--- Week 2/hi.c
+++ Week 2/hi.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
-    char c1 = 'H';
-    char c2 = 'I';
-    char c3 = '!';
+    string s = "Hi!";
 
-    printf("%i %i %i\n", (int) c1, (int) c2, (int) c3);
+    printf("%i %i %i %i\n", s[0], s[1], s[2], s[3]);
 }
