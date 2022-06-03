719f8831bfd8c78a18cd978cc69f5684b7104bdc
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-06-03 00:29:51 +0000

    Fri, Jun 3, 2022, 12:29 AM +00:00

--- Week 2/hi.c
+++ Week 2/hi.c
@@ -4,6 +4,8 @@
 int main(void)
 {
     string s = "HI!";
+    string t = "BYE!";
 
-    printf("%i %i %i %i %i %i %i\n", s[0], s[1], s[2], s[3], s[4], s[5], s[6]);
+    printf("%s\n", s);
+    printf("%s\n", t);
 }
