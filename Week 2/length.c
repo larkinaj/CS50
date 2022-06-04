30985ded3351e95985e1a23c42e022d54b26ec23
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-06-04 02:37:46 +0000

    Sat, Jun 4, 2022, 2:37 AM +00:00

--- Week 2/length.c
+++ Week 2/length.c
@@ -1,3 +1,24 @@
 #include <stdio.h>
 #include <cs50.h>
 
+int get_length(string s);
+
+int main(void)
+{
+    string name = get_string("Name: ");
+    int length = string_length(name);
+
+    printf("%i\n", length);
+}
+
+
+
+int string_length(string s)
+{
+    int i = 0;
+
+    while (name[1] != '\0')
+    {
+        i++;
+    }
+}
