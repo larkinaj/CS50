089b2dce6139b16790cfc44f7d06ed721fe7a635
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-06-06 22:48:19 +0000

    Mon, Jun 6, 2022, 10:48 PM +00:00

--- Week 2/string.c
+++ Week 2/string.c
@@ -8,8 +8,9 @@ int main(void)
     string s = get_string("Input: ");
 
     printf("Output: ");
-
-    for (int i = 0; i < strlen(s); i++)
+    int length = strlen(s);
+    
+    for (int i = 0; i < length; i++)
     {
         printf("%c", s[i]);
     }
