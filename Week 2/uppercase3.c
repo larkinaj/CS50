99a0334ebcb90f036214365cffe39a341d3989bc
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-06-06 23:34:29 +0000

    Mon, Jun 6, 2022, 11:34 PM +00:00

---
+++ Week 2/uppercase3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void)
+{
+    string s = get_string("Before: ");
+    printf("After: ");
+
+    for (int i = 0, n = strlen(s); i < n; i++)
+    {
+        if (isupper(s[i]))
+        {
+            printf("%c", tolower(s[i]));
+        }
+        else if (islower(s[i]))
+        {
+            printf("%c", toupper(s[i]));
+        }
+        else
+        {
+            printf("%c", s[i]);
+        }
+    }
+    printf("\n");
+}
+
