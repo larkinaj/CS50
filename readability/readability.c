7ba47db64fdf22dea0b86200d397ed22a985d61a
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-06-07 05:49:43 +0000

    Tue, Jun 7, 2022, 5:49 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -5,14 +5,17 @@
 
 int count_letters(string input);
 int count_words(string input);
+int count_sentences(string input);
 
 int main(void)
 {
     string input = get_string("Text: ");
     int length = count_letters(input);
     int words = count_words(input);
+    int sentences = count_sentences(input);
     printf("%i letters\n", length);
     printf("%i words\n", words);
+    printf("%i Sentences\n", sentences);
 }
 
 int count_letters(string input)
@@ -48,4 +51,21 @@ int count_words(string input)
     }
     words ++;
     return words;
+}
+
+int count_sentences(string input)
+{
+    int sentences = 0;
+    for (int i = 0, n = strlen(input); i < n; i++)
+    {
+        if (input[i] == '.' || input[i] == '!'|| input[i] == '?')
+        {
+            sentences ++;
+        }
+        else
+        {
+            sentences = sentences + 0;
+        }
+    }
+    return sentences;
 }
