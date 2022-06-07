ffaedc56f5d6e09ac88ecee71b2378873d942020
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-06-07 04:20:08 +0000

    Tue, Jun 7, 2022, 4:20 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -9,13 +9,13 @@ int main(void)
 {
     string input = get_string("Text: ");
     int length = count_letters(input);
-    printf("Length: %i\n");
+    printf("Length: %i\n", length);
 }
 
 int count_letters(string input)
 {
     for (int length = 0, n = strlen(input); length < n; length++)
-    {
+
         return length;
-    }
+
 }
