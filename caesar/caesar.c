e6e8411c6c27c782417e105441b60be365fa5390
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-06-07 14:29:09 +0000

    Tue, Jun 7, 2022, 2:29 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,7 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(int argc, string argv[]) 
+int main(int argc, string argv[])
 {
-
+    if (argc == 1)
+    {
+        printf("You need to add an argument\n");
+    }
+    else
+    {
+        printf("You typed an arg\n");
+    }
 }
