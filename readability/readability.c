f39912d3d65846ce7865068663656dacf4f3fc3b
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-06-07 04:30:52 +0000

    Tue, Jun 7, 2022, 4:30 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -14,7 +14,7 @@ int main(void)
 
 int count_letters(string input)
 {
-    
+    int length = strlen(input);
     for (int length = 0, n = strlen(input); length < n; length++)
     {
         printf("%c", input[n]);
