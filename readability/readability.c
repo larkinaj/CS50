39ce8b1dfb07f3b348dd53ce291a34307cfa9f93
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-06-07 04:02:29 +0000

    Tue, Jun 7, 2022, 4:02 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,14 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int count_letters(string text);
+int count_letters(string input);
 
 int main(void)
 {
     string input = get_string("Text: ");
 }
 
-int count_letters(string text)
+int count_letters(string input)
 {
     return 0;
 }
