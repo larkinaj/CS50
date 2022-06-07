c4518b4ad33a9d34f5707a6cb434d06f81069958
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-06-07 00:18:01 +0000

    Tue, Jun 7, 2022, 12:18 AM +00:00

--- Week 2/exit.c
+++ Week 2/exit.c
@@ -5,13 +5,14 @@
 
 int main(int argc, string argv[])
 {
-    if (argc == 2)
+    if (argc != 2)
     {
-        printf("Hello, %s!\n", argv[1]);
+        printf("Missing command-line argument.\n");
+        return 1;
     }
     else
     {
-        printf("Hello, world!\n");
+        printf("Hello, %s!\n", argv[1]);
     }
 
 }
