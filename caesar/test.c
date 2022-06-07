8a5da3355a99ed293f5bcabe91e48b3173eca65a
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-06-07 20:14:25 +0000

    Tue, Jun 7, 2022, 8:14 PM +00:00

---
+++ caesar/test.c
@@ -0,0 +1,43 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+bool only_digits(string key);
+
+int main(int argc, string argv[])
+{
+    string key = argv[1];
+    bool onlydigits = only_digits(key);
+    if (argc == 2 && onlydigits == true)
+    {
+        string plaintext = get_string("Plaintext: ");
+    }
+    else
+    {
+        printf("Usage: ./caesar key\n");
+        return 1;
+    }
+
+    int digit;
+    digit = atoi(key);
+    printf("New int: %i\n", digit);
+}
+
+bool only_digits(string key)
+{
+    bool onlydigits;
+    for (int i = 0; i < strlen(key); i++)
+    {
+        if (isdigit(key[i]))
+        {
+            onlydigits = true;
+        }
+        else
+        {
+            onlydigits = false;
+        }
+    }
+    return onlydigits;
+}
