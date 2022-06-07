b8982351f2d9fa82a558f13f83f82fe51f48d8af
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-06-07 15:04:48 +0000

    Tue, Jun 7, 2022, 3:04 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -5,14 +5,22 @@
 
 int main(int argc, string argv[])
 {
-    if (argc < 2 || argc > 2)
+    if (argc == 2)
     {
-        printf("Usage: ./caesar key\n");
-        return 1;
+        if (isdigit(argv[1]))
+        {
+            return 0;
+        }
+        else
+        {
+            printf("Usage: ./caesar key\n");
+            return 1;
+        }
     }
     else
     {
-        return 0;
+        printf("Usage: ./caesar key\n");
+        return 1;
     }
 
     if (isdigit(argv[1]))
