1e112ddee9d4107d8fe8c93d2f4369e9bab1e14c
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-06-22 23:50:28 +0000

    Wed, Jun 22, 2022, 11:50 PM +00:00

--- JavaScript/counter2.js
+++ JavaScript/counter2.js
@@ -13,5 +13,5 @@
 document.addEventListener('DOMContentLoaded', function(){
     document.querySelector('button').onclick = count;
 
-    setInterval(count, 1000);
+    // setInterval(count, 1000);
 });
