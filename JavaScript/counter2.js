bcf4e36abdf1978692648e96925952b0c7f097e4
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-06-22 23:46:41 +0000

    Wed, Jun 22, 2022, 11:46 PM +00:00

--- JavaScript/counter2.js
+++ JavaScript/counter2.js
@@ -1,9 +1,16 @@
+    if (!localStorage.getItem('counter')) {
+        localStorage.setItem('counter', 0);
+    }
+
+
     let counter = 0;
     function count() {
+        let counter = localStorage.getItem('counter');
         counter = counter + 1;
         document.querySelector('h1').innerHTML = counter ;
+        localStorage.setItem('counter', counter)
     }
-    
+
 document.addEventListener('DOMContentLoaded', function(){
     document.querySelector('button').onclick = count;
 
