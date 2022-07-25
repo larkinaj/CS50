75e0cc99b3ef36b83dc462a5b7dc100f6c7935f6
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-07-25 23:30:01 +0000

    Mon, Jul 25, 2022, 11:30 PM +00:00

---
+++ Week 3/numbers.c
@@ -0,0 +1,17 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+
+int main(void)
+{
+    int numbers[] = {4, 6, 8, 2, 7, 5, 0};
+
+    for (int i = 0; i < 7; i++) {
+        if (numbers[i] == 0) {
+            printf("Found\n");
+            return 0;
+        }
+    }
+}
