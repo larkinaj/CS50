2db6448ac4c8f129dd217c776e4351e51e6f9056
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-07-26 03:52:09 +0000

    Tue, Jul 26, 2022, 3:52 AM +00:00

--- Week 3/phonebook.c
+++ Week 3/phonebook.c
@@ -22,7 +22,6 @@ int main(void)
     for (int i = 0; i < 2; i++) {
         if (strcmp(people[i].name, "David") == 0) {
             printf("Found %s\n", people[i].number);
-            printf(person)
             return 0;
         }
     }
