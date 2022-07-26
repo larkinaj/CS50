f11f3870ea17465f0ea1ade375baabf9b5a8c6ed
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-07-26 03:19:08 +0000

    Tue, Jul 26, 2022, 3:19 AM +00:00

--- Week 3/phonebook.c
+++ Week 3/phonebook.c
@@ -15,14 +15,14 @@ int main(void)
     people[0].name = "Carter";
     people[0].number = "+1-617-495-1000";
 
-    people[0].name = "David";
-    people[0].number = "+1-949-468-2750";
+    people[1].name = "David";
+    people[1].number = "+1-949-468-2750";
 
     // string names[] = {"Carter", "David"};
     // string numbers[] = {"+1-617-495-1000", "+1-949-468-2750"};
 
     for (int i = 0; i < 2; i++) {
-        if (strcmp([i], "David") == 0) {
+        if (strcmp(people[i].name, "David") == 0) {
             printf("Found %s\n", numbers[i]);
             return 0;
         }
