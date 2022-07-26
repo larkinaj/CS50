9a920aeed13a15876c47244dc139ce2694aab0c7
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-07-26 23:22:57 +0000

    Tue, Jul 26, 2022, 11:22 PM +00:00

--- Week 3/iteration.c
+++ Week 3/iteration.c
@@ -17,8 +17,9 @@ void draw(int n)
 {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < i + 1; j++) {
-            printf("#\n");
+            printf("#");
         }
+        printf("\n");
     }
 }
 
