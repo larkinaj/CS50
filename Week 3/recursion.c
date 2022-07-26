3ac52a72120211f2d9635ac17c5d17e91fe172d2
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-07-26 23:28:15 +0000

    Tue, Jul 26, 2022, 11:28 PM +00:00

--- Week 3/recursion.c
+++ Week 3/recursion.c
@@ -15,10 +15,7 @@ int main(void)
 
 void draw(int n)
 {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < i + 1; j++) {
-            printf("#");
-        }
-        printf("\n");
+    if (n == height) {
+
     }
 }
