4cbae623329cf9d03ef6e44ee1b8cfffcaf62cf6
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-07-26 02:06:45 +0000

    Tue, Jul 26, 2022, 2:06 AM +00:00

---
+++ Week 3/names.c
@@ -0,0 +1,19 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+
+int main(void)
+{
+    string names[] = {"Bill", "Charlie", "Fred", "George", "Ginny", "Percy", "Ron"};
+
+    for (int i = 0; i < 7; i++) {
+        if (names[i] == "Ron") {
+            printf("Found\n");
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
+}
