178c29a05b1d67f6454d7ce35bed54ac0cf8635e
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-07-27 07:30:22 +0000

    Wed, Jul 27, 2022, 7:30 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -180,7 +180,12 @@ int find_min(void)
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].eliminated == false && candidates[i].votes = min) {
+            return true;
+        }
+    }
     return false;
 }
 
