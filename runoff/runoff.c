6707bc07dab2df883b16471dda1025e0a93c34e6
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-07-27 07:14:16 +0000

    Wed, Jul 27, 2022, 7:14 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -98,24 +98,24 @@ int main(int argc, string argv[])
         }
 
         // Eliminate last-place candidates
-        int min = find_min();
-        bool tie = is_tie(min);
+        //int min = find_min();
+        //bool tie = is_tie(min);
 
         // If tie, everyone wins
-        if (tie)
-        {
-            for (int i = 0; i < candidate_count; i++)
-            {
-                if (!candidates[i].eliminated)
-                {
-                    printf("%s\n", candidates[i].name);
-                }
-            }
-            break;
-        }
+        // if (tie)
+        // {
+        //     for (int i = 0; i < candidate_count; i++)
+        //     {
+        //         if (!candidates[i].eliminated)
+        //         {
+        //             printf("%s\n", candidates[i].name);
+        //         }
+        //     }
+        //     break;
+        // }
 
         // Eliminate anyone with minimum number of votes
-        eliminate(min);
+        // eliminate(min);
 
         // Reset vote counts back to zero
         for (int i = 0; i < candidate_count; i++)
