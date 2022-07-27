1778e0a1ebf4b9dd23998cba3c84d1f562acbc51
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-07-27 17:50:14 +0000

    Wed, Jul 27, 2022, 5:50 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -144,7 +144,7 @@ bool vote(int voter, int rank, string name)
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
-    int votingRound = 0;
+    int currentCandidates = candidate_count;
     for (int i = 0; i < candidate_count; i++)
     {
         if (candidates[i].eliminated) {
