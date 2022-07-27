cba6e34957e7a2fa37e48217c21b8730a27e4b1d
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-07-27 18:42:56 +0000

    Wed, Jul 27, 2022, 6:42 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -173,7 +173,7 @@ bool print_winner(void)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].votes > voter_count / 2) {
+        if (candidates[i].votes > voter_count / 2 && !candidates[i].eliminated) {
             printf("%s\n", candidates[i].name);
             return true;
         }
