e9bdc14041a9f90afbb5877a6b7580436e2fed75
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-07-27 17:13:54 +0000

    Wed, Jul 27, 2022, 5:13 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -142,15 +142,16 @@ bool vote(int voter, int rank, string name)
 void tabulate(void)
 {
     int votingRound = 0;
-    
+
     for (int i = 0; i < voter_count; i++) {
         for (int j = 0; j < candidate_count; j++) {
-                if (preferences[i][0] == j) {
+                if (preferences[i][votingRound] == j) {
                     candidates[j].votes += 1;
                 }
                 printf("Candidate: %s and votes: %i and elim: %d\n",candidates[j].name, candidates[j].votes, candidates[j].eliminated);
         }
     }
+    votingRound ++;
     return;
 }
 
