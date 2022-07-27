481f4ee57b1f48d1a8370f41b42fa18d7dc5b061
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-07-27 05:31:03 +0000

    Wed, Jul 27, 2022, 5:31 AM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -68,9 +68,7 @@ bool vote(string name)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        printf("%s", name);
         if (strcmp(candidates[i].name, name) == 0) {
-        // if (candidates[i].name == name) {
             candidates[i].votes += 1;
             return true;
         }
