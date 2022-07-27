9bc116aeea6be71cdd84beb85e6aaa75441ee4dc
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-07-27 20:57:16 +0000

    Wed, Jul 27, 2022, 8:57 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -102,7 +102,7 @@ int main(int argc, string argv[])
         // Eliminate last-place candidates
         int min = find_min();
         bool tie = is_tie(min);
-        printf("bool tie: %d (1==true, 0==false)\n", tie);
+
         // If tie, everyone wins
         if (tie)
         {
