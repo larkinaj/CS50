f864fd903401f49f8727647bc0564864fcaa120d
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-07-27 06:18:02 +0000

    Wed, Jul 27, 2022, 6:18 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -130,7 +130,7 @@ bool vote(int voter, int rank, string name)
     for (int i = 0; i < candidate_count; i++)
     {
         if (strcmp(candidates[i].name, name) == 0) {
-            candidates[i].votes += 1;
+            preferences[i][j]
             return true;
         }
     }
