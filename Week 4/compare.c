aeda2269b4c9bc027f04a541b2165e001d6bb180
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-08-21 03:39:09 +0000

    Sun, Aug 21, 2022, 3:39 AM +00:00

--- Week 4/compare.c
+++ Week 4/compare.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <string.h>
 
 int main(void)
 {
     char *i = get_string("i: ");
     char *j = get_string("j: ");
 
-    if (i == j) {
+    // if (i == j) {
+    //     printf("Same\n");
+    // }
+    // else {
+    //     printf("Different\n");
+    // }
+
+    if (strcmp(i, j) == 0) {
         printf("Same\n");
     }
     else {
