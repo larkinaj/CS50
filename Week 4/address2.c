e4b884733fa2d041893d1bdb40c387a11165a753
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-08-21 03:15:26 +0000

    Sun, Aug 21, 2022, 3:15 AM +00:00

--- Week 4/address2.c
+++ Week 4/address2.c
@@ -5,12 +5,10 @@
 int main(void)
 {
     string s = "HI!";
-    char *x = &s[0];
 
-    printf("Address of %s is: %p\n" , s, s);
-    printf("Address of the first char in s is: %p\n",&s[0]);
-    printf("Address of the second char in s is: %p\n",&s[1]);
-    printf("Address of the third char in s is: %p\n",&s[2]);
-    printf("Address of the fourth char in s is: %p\n",&s[3]);
+    printf("%c\n" , s[0]);
+    printf("%c\n" , s[1]);
+    printf("%c\n" , s[2]);
+    printf("%c\n" , s[3]);
 
 }
