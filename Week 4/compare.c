9eaf70becf76a52369895d371053dafb6a918f96
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-08-21 03:45:10 +0000

    Sun, Aug 21, 2022, 3:45 AM +00:00

--- Week 4/compare.c
+++ Week 4/compare.c
@@ -4,18 +4,23 @@
 
 int main(void)
 {
-    char *i = get_string("i: ");
-    char *j = get_string("j: ");
-    
-    // the below code doesnt work because
-    // if (i == j) {
-    //     printf("Same\n");
-    // }
-    // else {
-    //     printf("Different\n");
-    // }
+    char *s = get_string("s: ");
+    char *t = get_string("t: ");
 
-    if (strcmp(i, j) == 0) {
+    int i[] = {1};
+    int j[] = {1};
+
+    //the below code doesnt work because != and == will only
+    //compare the base addresses of those strings. Not the contents of the strings themselves.
+    if (i == j) {
+        printf("Same\n");
+    }
+    else {
+        printf("Different\n");
+    }
+
+
+    if (strcmp(s, t) == 0) {
         printf("Same\n");
     }
     else {
