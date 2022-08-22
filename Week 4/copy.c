063ac3e76900efff4261967afdc3fd03b563c15f
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-08-22 02:34:14 +0000

    Mon, Aug 22, 2022, 2:34 AM +00:00

--- Week 4/copy.c
+++ Week 4/copy.c
@@ -18,7 +18,8 @@
 int main(void)
 {
     char *s = get_string("Enter s: ");
-    char *t = malloc(strlen(s) + 1);
+    //char *t = malloc(strlen(s) + 1);
+    char t[2];
 
     // // for (int i = 0; i < strlen(s) + 1; i++) {
     // //     t[i] = s[i];
@@ -32,5 +33,5 @@ int main(void)
 
     printf("s: %s\n", s);
     printf("t: %s\n", t);
-    free(t);
+    //free(t);
 }
