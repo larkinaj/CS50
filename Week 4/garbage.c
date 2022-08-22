4968952bb9c3b8d3283e3d863bd97a7a4d53a15f
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-08-22 03:05:23 +0000

    Mon, Aug 22, 2022, 3:05 AM +00:00

--- Week 4/garbage.c
+++ Week 4/garbage.c
@@ -4,8 +4,11 @@
 int main(void)
 {
 
-    int scores[3];
-    for (int i = 0; i < sizeof(scores); i++) {
+    int scores[3]= {1,2,3};
+    int s = sizeof(scores);
+    //for (int i = 0; i < sizeof(scores); i++) {
+    for (int i = 0; i < 3; i++) {
         printf("%i\n", scores[i]);
     }
+    printf("size: %i\n", s);
 }
