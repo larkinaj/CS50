7c247c6aef925fee59b0fcf90219604bca9a84af
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-08-22 00:40:20 +0000

    Mon, Aug 22, 2022, 12:40 AM +00:00

--- Week 4/copy.c
+++ Week 4/copy.c
@@ -3,10 +3,21 @@
 #include <string.h>
 #include <ctype.h>
 
+// int main(void)
+// {
+//     string s = get_string("Enter s: ");
+//     string t = s;
+
+//     t[0] = toupper(t[0]);
+
+//     printf("s: %s\n", s);
+//     printf("t: %s\n", t);
+// }
+
 int main(void)
 {
-    string s = get_string("s: ");
-    string t = s;
+    char *s = get_string("Enter s: ");
+    char *t = malloc();
 
     t[0] = toupper(t[0]);
 
