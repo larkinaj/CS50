8f3316e880e3c497fc793628b1781a7ee9605f12
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-08-22 00:46:25 +0000

    Mon, Aug 22, 2022, 12:46 AM +00:00

--- Week 4/copy.c
+++ Week 4/copy.c
@@ -2,6 +2,7 @@
 #include <cs50.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdlib.h>
 
 // int main(void)
 // {
@@ -19,11 +20,11 @@ int main(void)
     char *s = get_string("Enter s: ");
     char *t = malloc(strlen(s) + 1);
 
-    // for (int i = 0; i < strlen(s) + 1; i++) {
-    //     t[i] = s[i];
-    // }
+    for (int i = 0; i < strlen(s) + 1; i++) {
+        t[i] = s[i];
+    }
 
-    strcopy(t,s);
+    //strcpy(t,s);
 
     t[0] = toupper(t[0]);
 
