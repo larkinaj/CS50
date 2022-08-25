d062965060eb0a87681e31f0cae32d0e832961e3
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-08-25 20:14:26 +0000

    Thu, Aug 25, 2022, 8:14 PM +00:00

--- Week 4/scanf.c
+++ Week 4/scanf.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 
 
+// int main(void)
+// {
+
+//     int x;
+//     printf("x: ");
+//     scanf("%i", &x);
+//     printf("x: %i\n", x);
+// }
+
+
 int main(void)
 {
 
-    int x;
-    printf("x: ");
-    scanf("%i", &x);
-    printf("x: %i\n", x);
+    char *s;
+    printf("s: ");
+    scanf("%s", s);
+    printf("s: %s\n", s);
 }
 
