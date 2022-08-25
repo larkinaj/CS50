fac7b3fe934206f91bf12d9e6d542914943e7c9b
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-08-25 05:37:04 +0000

    Thu, Aug 25, 2022, 5:37 AM +00:00

--- Week 4/swap.c
+++ Week 4/swap.c
@@ -10,15 +10,13 @@ int main(void)
     int y = 2;
 
     printf("x is %i, y is %i\n", x, y);
-    swap(x, y);
+    swap(&x, &y);
     printf("x is %i, y is %i\n", x, y);
 }
 
 
 void swap(int *a, int *b) {
-    printf("a is %i, b is %i\n", a, b);
     int tmp = *a;
     *a = *b;
     *b = tmp;
-    printf("a is %i, b is %i\n", a, b);
 }
