f6e78ac297a6852b0ffb6f6ba03d7c0f3abd70fc
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-08-31 17:35:18 +0000

    Wed, Aug 31, 2022, 5:35 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -3,9 +3,10 @@
 
 int main(int argc, char *argv[])
 {
- if (argc != 2)
+    if (argc != 2)
     {
         printf("Usage: ./recover IMAGE\n");
         return 1;
     }
+    FILE *file = fopen(argv[1], "r");
 }
