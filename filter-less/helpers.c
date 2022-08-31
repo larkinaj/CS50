5d0689975d44e8a96e2c3fdf75c31b4a35e49f78
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-08-31 05:01:12 +0000

    Wed, Aug 31, 2022, 5:01 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -59,29 +59,14 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     // nested for loop to iterate through each pixel of the image
     for (int i = 0; i < height; i++) {
-        
         for (int j = 0; j < width; j++) {
             int red = image[i][j].rgbtRed;
             int green = image[i][j].rgbtGreen;
             int blue = image[i][j].rgbtBlue;
 
-            int sepiaRed = round((red * 0.393) + (green * 0.769) + (blue * 0.189));
-            int sepiaGreen = round((red * 0.349) + (green * 0.686) + (blue * 0.168));
-            int sepiaBlue = round((red * 0.272) + (green * 0.534) + (blue * 0.131));
-
-            if  (sepiaRed > 255) {
-                sepiaRed = 255;
-            }
-            if  (sepiaGreen > 255) {
-                sepiaGreen = 255;
-            }
-            if  (sepiaBlue > 255) {
-                sepiaBlue = 255;
-            }
-
-            image[i][j].rgbtRed = sepiaRed;
-            image[i][j].rgbtGreen = sepiaGreen;
-            image[i][j].rgbtBlue = sepiaBlue;
+            image[i][j].rgbtRed = image[i][width - 1 - j].rgbtRed;
+            image[i][j].rgbtGreen = image[i][width - 1 - j].rgbtGreen;
+            image[i][j].rgbtBlue = image[i][width - 1 - j].rgbtBlue;
         }
     }
     return;
