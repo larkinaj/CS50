5b8833fe10ceaf739bb1c78b56288c745fdfee3b
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-08-31 03:47:39 +0000

    Wed, Aug 31, 2022, 3:47 AM +00:00

--- Week 4/scanf.c
+++ Week 4/scanf.c
@@ -10,13 +10,21 @@
 // }
 
 
-int main(void)
+// int main(void)
+// {
+
+//     char *s = malloc(4);
+//     printf("s: ");
+//     scanf("%s", s);
+//     printf("s: %s\n", s);
+//     free(s);
+// }
+
+
+int main()
 {
-
-    char *s = malloc(4);
-    printf("s: ");
-    scanf("%s", s);
-    printf("s: %s\n", s);
-    free(s);
+    int a;
+    scanf("This is the value %d", &a);
+    printf("Input value read : a = %d", a);
+    return 0;
 }
-
