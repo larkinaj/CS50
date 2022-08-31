b833782098927e60766bec1caa2b168bce1ba5a1
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-08-31 04:42:40 +0000

    Wed, Aug 31, 2022, 4:42 AM +00:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -25,17 +25,20 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    // nested for loop to iterate through each pixel of the image
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
             float red = image[i][j].rgbtRed;
             float green = image[i][j].rgbtGreen;
             float blue = image[i][j].rgbtBlue;
 
-            int average = round((red + green + blue) / sizeof(RGBTRIPLE));
+            int sepiaRed = round((red * 0.393) + (green * 0.769) + (blue * 0.189));
+            int sepiaGreen = round((red * 0.349) + (green * 0.686) + (blue * 0.168));
+            int sepiaBlue = round((red * 0.272) + (green * 0.534) + (blue * 0.131));
 
-            image[i][j].rgbtRed = average;
-            image[i][j].rgbtGreen = average;
-            image[i][j].rgbtBlue = average;
+            image[i][j].rgbtRed = sepiaRed;
+            image[i][j].rgbtGreen = sepiaGreen;
+            image[i][j].rgbtBlue = sepiaBlue;
         }
     }
     return;
@@ -44,6 +47,22 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    // nested for loop to iterate through each pixel of the image
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            float red = image[i][j].rgbtRed;
+            float green = image[i][j].rgbtGreen;
+            float blue = image[i][j].rgbtBlue;
+
+            int sepiaRed = round((red * 0.393) + (green * 0.769) + (blue * 0.189));
+            int sepiaGreen = round((red * 0.349) + (green * 0.686) + (blue * 0.168));
+            int sepiaBlue = round((red * 0.272) + (green * 0.534) + (blue * 0.131));
+
+            image[i][j].rgbtRed = sepiaRed;
+            image[i][j].rgbtGreen = sepiaGreen;
+            image[i][j].rgbtBlue = sepiaBlue;
+        }
+    }
     return;
 }
 
