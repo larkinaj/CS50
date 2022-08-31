12d54641ce3d3e535f82568e13c271526bf3e9fd
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-08-31 03:54:25 +0000

    Wed, Aug 31, 2022, 3:54 AM +00:00

--- Week 4/scanf.c
+++ Week 4/scanf.c
@@ -13,10 +13,9 @@
 int main(void)
 {
 
-    // char *s = malloc(4);
-    char *s;
+    char *s = malloc(4);
     printf("s: ");
-    scanf("%s", &s);
+    scanf("%s", s);
     printf("s: %s\n", s);
     free(s);
 }
