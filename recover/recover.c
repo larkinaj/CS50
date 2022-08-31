903cc9470964b49999f8517e3297aca1237a44f6
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-08-31 17:44:27 +0000

    Wed, Aug 31, 2022, 5:44 PM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -10,9 +10,9 @@ int main(int argc, char *argv[])
     }
     FILE *file = fopen(argv[1], "r");
 
-    while (fread(buffer, 1, BLOCK_SIZE, raw_file) == BLOCK_SIZE)
-    {
+    // while (fread(buffer, 1, BLOCK_SIZE, raw_file) == BLOCK_SIZE)
+    // {
 
 
-    }
+    // }
 }
