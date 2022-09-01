079ed7f9f662cd1294c7fa2e2ffa2f54a5d06284
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-09-01 02:37:02 +0000

    Thu, Sep 1, 2022, 2:37 AM +00:00

--- recover/recover.c
+++ recover/recover.c
@@ -11,28 +11,34 @@ int main(int argc, char *argv[])
         printf("Usage: ./recover IMAGE\n");
         return 1;
     }
-    FILE *file = fopen(argv[1], "r");
 
-    int images = 0;
-    unsigned char buffer[512];
+    FILE *file = fopen(argv[1], "r");
     FILE *output = NULL;
+    unsigned char buffer[512];
+    int images = 0;
     char *filename = malloc(8 * sizeof(char));
+
     while (fread(buffer, sizeof(char), 512, file))
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
+            if (images > 0)
+            {
+                fclose(output);
+            }
+
             sprintf(filename, "%03i.jpg", images);
 
             output = fopen(filename, "w");
 
             images ++;
         }
+
         if (output != NULL)
         {
             fwrite(buffer, sizeof(char), 512, output);
         }
     }
-
 
     free(filename);
     fclose(output);
