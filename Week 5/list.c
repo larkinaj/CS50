81cf195134a659afd18fa16880c02cdce9a7d8a6
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-09-01 03:01:07 +0000

    Thu, Sep 1, 2022, 3:01 AM +00:00

--- Week 5/list.c
+++ Week 5/list.c
@@ -10,26 +10,27 @@ int main(void)
     // list[1] = 2;
     // list[2] = 3;
 
-    // for (int i = 0; i < 3; i++)
-    // {
-    //     printf("%i\n", list[i]);
-    // }
-
     int *list = malloc(3 * sizeof(int));
 
     list[0] = 1;
     list[1] = 2;
     list[2] = 3;
 
-    int *tmp = malloc(4 * sizeof(int)):
+    int *tmp = malloc(4 * sizeof(int));
 
-    list[3] = 4;
-    
     for (int i = 0; i < 3; i++)
+    {
+        tmp[i] = list[i];
+    }
+    tmp[3] = 4;
+    free(list);
+
+    list = tmp;
+
+    for (int i = 0; i < 4; i++)
     {
         printf("%i\n", list[i]);
     }
-
 
     free(list);
 }
