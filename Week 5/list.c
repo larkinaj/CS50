37aa8f750b38eb58b0ed360b6480fc22c0c5f882
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-09-02 01:35:18 +0000

    Fri, Sep 2, 2022, 1:35 AM +00:00

--- Week 5/list.c
+++ Week 5/list.c
@@ -2,31 +2,38 @@
 #include <stdlib.h>
 
 
+// int main(void)
+// {
+//     int *list = malloc(3 * sizeof(int));
+
+//     list[0] = 1;
+//     list[1] = 2;
+//     list[2] = 3;
+
+//     int *tmp = realloc(list, 4 * sizeof(int));
+
+//     tmp[3] = 4;
+
+//     list = tmp;
+
+//     for (int i = 0; i < 4; i++)
+//     {
+//         printf("%i\n", list[i]);
+//     }
+
+//     free(list);
+// }
+
+typedef struct node
+{
+    int number;
+    struct node *next;
+}
+node;
+
 int main(void)
 {
-    // int list[3];
+    // list of size 0
+    node *list = NULL
 
-    // list[0] = 1;
-    // list[1] = 2;
-    // list[2] = 3;
-
-    int *list = malloc(3 * sizeof(int));
-
-    list[0] = 1;
-    list[1] = 2;
-    list[2] = 3;
-
-    int *tmp = realloc(list, 4 * sizeof(int));
-
-    tmp[3] = 4;
-
-    list = tmp;
-
-    for (int i = 0; i < 4; i++)
-    {
-        printf("%i\n", list[i]);
-    }
-
-    free(list);
 }
-
