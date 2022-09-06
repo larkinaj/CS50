9513ffc1ac922a109c3368ca67f37979d9d4835f
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-09-06 17:16:57 +0000

    Tue, Sep 6, 2022, 5:16 PM +00:00

--- Week 6/calculator.py
+++ Week 6/calculator.py
@@ -1,6 +1,8 @@
 # from cs50 import get_int
 import cs50
 
-x = cs50.get_int("x: ")
-y = cs50.get_int("y: ")
+# x = cs50.get_int("x: ")
+# y = cs50.get_int("y: ")
+x = int(input("x: "))
+y = int(input("y: "))
 print(x + y)
