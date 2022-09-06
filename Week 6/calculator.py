29eb0015a09d351b923ab456bec40bc4638e6f1e
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-09-06 17:28:08 +0000

    Tue, Sep 6, 2022, 5:28 PM +00:00

--- Week 6/calculator.py
+++ Week 6/calculator.py
@@ -1,5 +1,12 @@
 
-
-x = int(input("x: "))
-y = int(input("y: "))
+try:
+    x = int(input("x: "))
+except:
+    print("That is not an int")
+    exit()
+try:
+    y = int(input("y: "))
+except:
+    print("That is not an int")
+    exit()
 print(x + y)
