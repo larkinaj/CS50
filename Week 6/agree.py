1d02b53f4a58a6fc840f3fc959aecdff4ea5447c
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-09-06 19:10:48 +0000

    Tue, Sep 6, 2022, 7:10 PM +00:00

--- Week 6/agree.py
+++ Week 6/agree.py
@@ -1,3 +1,7 @@
-agree = input("Do you agree? ").tolower()
-print(agree)
-# if agree == "
+agree = input("Do you agree? ").lower()
+if agree == "y":
+    print("You have agreed")
+elif agree == "n":
+    print("You have not agreed")
+else:
+    print("You have not entered a valid character")
