82b63245dd9440bac97a6c3035e4281db4b75f77
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-09-06 16:25:35 +0000

    Tue, Sep 6, 2022, 4:25 PM +00:00

--- Week 6/blur.py
+++ Week 6/blur.py
@@ -1,5 +1,5 @@
-form PIL import Image, ImageFilter
+from PIL import Image, ImageFilter
 
-before = Image.open("bridge.bmp")
+before = Image.open("tower.bmp")
 after = before.filter(ImageFilter.BoxBlur(1))
 after.save("out.bmp")
