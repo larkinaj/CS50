15475b00c3f8d52ac5935b580cf9fd28818094d7
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-09-07 15:22:18 +0000

    Wed, Sep 7, 2022, 3:22 PM +00:00

--- Week 6/argv.py
+++ Week 6/argv.py
@@ -1,6 +1,10 @@
 from sys import argv
 
-if len(argv) == 2:
-    print(f"hello, " + argv[1])
-else:
-    print("hello, world")
+# if len(argv) == 2:
+#     print(f"hello, " + argv[1])
+# else:
+#     print("hello, world")
+
+
+for agr in agrv:
+    print(arg)
