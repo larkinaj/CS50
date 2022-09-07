28e0049ab472542a3046575062f202d59712d3ec
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-09-07 14:52:05 +0000

    Wed, Sep 7, 2022, 2:52 PM +00:00

--- Week 6/mario.py
+++ Week 6/mario.py
@@ -1,18 +1,21 @@
-from cs50 import get_int
+# def main():
+#     n = height()
+#     for i in range(n):
+#         print("#")
+
+# def height():
+#     while True:
+#         try:
+#             n = int(input("Height: "))
+#             if n > 0:
+#                 return n
+#         except ValueError:
+#             print("That's not an integer")
+
+# main()
 
 
-def main():
-    n = height()
-    for i in range(n):
-        print("#")
 
-def height():
-    while True:
-        try:
-            n = int(input("Height: "))
-            if n > 0:
-                return n
-        except ValueError:
-            print("That's not an integer")
 
-main()
+for i in range(4):
+    print("?", "")
