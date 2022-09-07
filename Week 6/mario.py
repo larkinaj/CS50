f134a95483dc416f37de73e637b0695d1e370ce9
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-09-07 14:38:57 +0000

    Wed, Sep 7, 2022, 2:38 PM +00:00

--- Week 6/mario.py
+++ Week 6/mario.py
@@ -2,7 +2,7 @@ from cs50 import get_int
 
 while True:
     n = int(input("Height: "))
-    if n > 0 || type(n) != "int"
+    if n > 0 or type(n) != "int":
         break
 
 # n = get_int("Height: ")
