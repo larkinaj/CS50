df64d796cf71aa63078564af5f36f9f5da5f3c77
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-09-08 16:10:04 +0000

    Thu, Sep 8, 2022, 4:10 PM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -26,20 +26,20 @@ def main():
             matches.append("unmatch")
         strList.append(longest_match(dna, firstRow[i]))
 
-    # print(strList)
+    print(strList)
 
     # todo: Check database for matching profiles
     for row in csv_reader:
         for i in range(1, len(row)):
-            # print(i)
-            # print(f"str: {strList[i]}")
-            # print(f"row: {row[i]}")
+            print(i)
+            print(f"str: {strList[i]}")
+            print(f"row: {row[i]}")
             if int(row[i]) == strList[i]:
                 matches[i] = "match"
             if matches[len(row) - 1] == "match":
                 print(row[0])
-                return
-    # print(matches)
+                # return
+    print(matches)
     # if "unmatch" not in matches:
     #     print("we have a match!!!!!!!!!!")
     print("No match")
