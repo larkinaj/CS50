7d416b34c6364823c224c3fa067d1e70ffac9bba
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-09-08 05:39:48 +0000

    Thu, Sep 8, 2022, 5:39 AM +00:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -4,8 +4,10 @@ from cs50 import get_int
 while True:
     try:
         height = get_int("Height: ")
-        if n > 0 and n < 8:
+        if height > 0 and height < 9:
             break
+    except ValueError:
+            print("That's not an integer!")
 
 
 for i in range(1, height + 1):
