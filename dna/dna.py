48e2d05728c99b515fd3ae3e57bebdb7515fa54c
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-09-08 14:14:49 +0000

    Thu, Sep 8, 2022, 2:14 PM +00:00

--- dna/dna.py
+++ dna/dna.py
@@ -4,7 +4,7 @@ import sys
 
 def main():
 
-    # todo: Check for command-line usage
+    # Check for command-line usage
     if len(sys.argv) != 3:
         print("Usage: python dna.py data.csv sequence.txt")
         sys.exit(1)
