c8c8544f6566e3f0823ba165137918c3b760f992
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-10-12 04:53:49 +0000

    Wed, Oct 12, 2022, 4:53 AM +00:00

--- Week 7/favorites.py
+++ Week 7/favorites.py
@@ -52,7 +52,7 @@ with open("favorites.csv", "r") as file:
     reader = csv.DictReader(file)
     for row in reader:
         title = row["title"].strip().upper()
-        if re.search("office", title):
+        if re.search("^(OFFICE|THE OFFICE)$", title):
             counter += 1
 
 print(f"Number of people who like the office: {counter}")
