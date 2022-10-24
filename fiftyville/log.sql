1c8d089627ade95630a37638a8a248f801dc783a
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-10-24 22:42:35 +0000

    Mon, Oct 24, 2022, 10:42 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -2,7 +2,12 @@
 
 -- Used to check the tables in the db
 .tables
+
 --Used to check the schema of crime_scene_reports
 .schema crime_scene_reports
+
 --Used to check the crime scene report for the duck theft incident
 SELECT description FROM crime_scene_reports WHERE month = 7 AND day = 28 AND street = "Humphrey Street"
+
+-- Used to check the schema of bakery_security_logs
+.schema bakery_security_logs
