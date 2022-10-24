639af7caeb5da3336c270c6493220e8b001ff6d2
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-10-24 22:37:40 +0000

    Mon, Oct 24, 2022, 10:37 PM +00:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,8 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- Used to check the tables in the db
+.tables
+--Used to check the schema of crime_scene_reports
+.schema crime_scene_reports
+--Used to check the crime scene report for the duck theft incident
+SELECT description FROM crime_scene_reports WHERE month = 7 AND day = 28 AND street = "Humphrey Street"
