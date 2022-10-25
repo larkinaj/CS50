5a7f5ff9d592430cf75967eeb9742c93f3db2d07
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-10-25 04:27:39 +0000

    Tue, Oct 25, 2022, 4:27 AM +00:00

--- homepage/index.js
+++ homepage/index.js
@@ -1,13 +1,27 @@
-// document.getElementById("test").onclick = function () {
-//     console.log("it clicked!")
-//     window.location.href = "china.html";
-// };
-// test = document.getElementById("test");
+
+test = document.getElementsByClassName("card1")[0]
+test1 = document.getElementsByClassName("card1")
 
 
-document.getElementsByClassName("card1").onclick = function () {
+document.getElementsByClassName("card1")[0].onclick = function () {
     console.log("it clicked!")
     window.location.href = "china.html";
 };
 
-console.log("test")
+document.getElementsByClassName("card2")[0].onclick = function () {
+    console.log("it clicked!")
+    window.location.href = "india.html";
+};
+
+document.getElementsByClassName("card3")[0].onclick = function () {
+    console.log("it clicked!")
+    window.location.href = "japan.html";
+};
+
+document.getElementsByClassName("card4")[0].onclick = function () {
+    console.log("it clicked!")
+    window.location.href = "italy.html";
+};
+
+console.log(test)
+console.log(test1)
