85273fc554b3d73831186a5a07aba310dfb0e599
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-10-25 04:21:38 +0000

    Tue, Oct 25, 2022, 4:21 AM +00:00

--- homepage/index.js
+++ homepage/index.js
@@ -1,8 +1,13 @@
-document.getElementById("test").onclick = function () {
+// document.getElementById("test").onclick = function () {
+//     console.log("it clicked!")
+//     window.location.href = "china.html";
+// };
+// test = document.getElementById("test");
+
+
+document.getElementsByClassName("card1").onclick = function () {
     console.log("it clicked!")
     window.location.href = "china.html";
 };
-test = document.getElementById("test");
 
-console.log(test)
 console.log("test")
