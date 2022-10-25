5cb747191d75e4dc11f6883d1885ad1fd9dd5998
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-10-25 04:40:14 +0000

    Tue, Oct 25, 2022, 4:40 AM +00:00

--- homepage/index.js
+++ homepage/index.js
@@ -9,16 +9,13 @@ document.getElementsByClassName("card1")[0].onclick = function () {
 };
 
 document.getElementsByClassName("card2")[0].onclick = function () {
-    console.log("it clicked!")
     window.location.href = "india.html";
 };
 
 document.getElementsByClassName("card3")[0].onclick = function () {
-    console.log("it clicked!")
     window.location.href = "japan.html";
 };
 
 document.getElementsByClassName("card4")[0].onclick = function () {
-    console.log("it clicked!")
     window.location.href = "italy.html";
 };
