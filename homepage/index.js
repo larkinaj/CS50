5a831a516ef6bb7a3ff21bf9aa5a1ec74f9b6c18
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-10-25 05:03:41 +0000

    Tue, Oct 25, 2022, 5:03 AM +00:00

--- homepage/index.js
+++ homepage/index.js
@@ -1,48 +1,41 @@
 
-const locationButtons = function(location) {
-    window.location.href = location + ".html"
-};
 
-const getInnerText = function(element) {
-    return element.innerText
+if (window.location.pathname === "/index.html") {
+    document.getElementsByClassName("card1")[0].onclick = function () {
+        window.location.href = "china.html";
+    };
+
+    document.getElementsByClassName("card2")[0].onclick = function () {
+        window.location.href = "india.html";
+    };
+
+    document.getElementsByClassName("card3")[0].onclick = function () {
+        window.location.href = "japan.html";
+    };
+
+    document.getElementsByClassName("card4")[0].onclick = function () {
+        window.location.href = "italy.html";
+    };
 }
 
-document.getElementsByClassName("card1")[0].onclick = function () {
-    locationButtons("china");
-};
-
-document.getElementsByClassName("card2")[0].onclick = function () {
-    window.location.href = "india.html";
-};
-
-document.getElementsByClassName("card3")[0].onclick = function () {
-    window.location.href = "japan.html";
-};
-
-document.getElementsByClassName("card4")[0].onclick = function () {
-    window.location.href = "italy.html";
-};
-
 document.getElementsByClassName("nav-link")[0].onclick = function () {
-    locationButtons("index");
+    window.location.href = "index.html";
 };
 
 document.getElementsByClassName("nav-link")[1].onclick = function () {
-    locationButtons("china");
+    window.location.href = "china.html";
 };
 
 document.getElementsByClassName("nav-link")[2].onclick = function () {
-    locationButtons("india");
+    window.location.href = "india.html";
 };
 
 document.getElementsByClassName("nav-link")[3].onclick = function () {
-    locationButtons("japan");
+    window.location.href = "japan.html";
 };
 
 document.getElementsByClassName("nav-link")[4].onclick = function () {
-    locationButtons("italy");
+    window.location.href = "italy.html";
 };
 
-console.log(document.getElementsByClassName("nav-link")[0].innerText.toLowerCase())
-console.log(document.getElementsByClassName("nav-link"))
-console.log(window.location)
+console.log(window.location.pathname)
