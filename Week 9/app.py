c6d39483dce4aa4c803635b2e99c420d5d1f0f59
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-10-29 02:36:28 +0000

    Sat, Oct 29, 2022, 2:36 AM +00:00

--- Week 9/app.py
+++ Week 9/app.py
@@ -1,14 +1,20 @@
 from flask import Flask, render_template, request
 
 
+# app = Flask(__name__)
+
+
+
+# @app.route("/")
+# def index():
+#     name = request.args.get("name")
+#     return render_template("index.html", name=name)
+########################################################
+
 app = Flask(__name__)
 
 
 
 @app.route("/")
 def index():
-    name = request.args.get("name")
-    return render_template("index.html", name=name)
-
-
-    
+    return render_template("index.html")
