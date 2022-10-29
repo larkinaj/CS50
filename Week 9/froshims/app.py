10f51bae27e7190aba170265cd28aa0b26805ddb
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-10-29 20:26:37 +0000

    Sat, Oct 29, 2022, 8:26 PM +00:00

--- Week 9/froshims/app.py
+++ Week 9/froshims/app.py
@@ -7,7 +7,7 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/greet")
+@app.route("/register")
 def greet():
     # Validate submission
     if not request.form.get("name") or request.form.get("sport") not in ["Basketball", "Soccer", "Ultimate Frisbee"]
