6d29805d09ed2816aa1e0edd61c1653f5f8b63b6
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-10-29 21:41:50 +0000

    Sat, Oct 29, 2022, 9:41 PM +00:00

--- Week 9/froshims/app.py
+++ Week 9/froshims/app.py
@@ -22,4 +22,9 @@ def register():
     if not request.form.get("name") or request.form.get("sport") not in SPORTS:
         return render_template("failure.html")
 
+    name = request.form.get("name")
+    sport = request.form.get("sport")
+
+    REGISTRANTS[name] = sport
+
     return render_template("success.html")
