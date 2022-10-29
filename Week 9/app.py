87b457fabbd8ec366c3b14744270f1536ed9b8b1
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-10-29 19:48:49 +0000

    Sat, Oct 29, 2022, 7:48 PM +00:00

--- Week 9/app.py
+++ Week 9/app.py
@@ -56,7 +56,7 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/greet")
+@app.route("/greet", methods=["POST"])
 def greet():
     name = request.args.get("name")
     return render_template("greet.html", name=name)
