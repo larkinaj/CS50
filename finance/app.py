2fd62a3ab8fb066c7d9ef46e14df5920ce502250
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-10-31 03:23:27 +0000

    Mon, Oct 31, 2022, 3:23 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -113,10 +113,9 @@ def quote():
     """Get stock quote."""
     if request.method =="POST":
         symbol = request.form.get("symbol")
-        print(symbol)
         if lookup(symbol) == None:
             return apology("Could not find that symbol")
-        return render_template("quoted.html")
+        return render_template("quoted.html", )
 
     return render_template("quote.html")
 
