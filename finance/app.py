ddc681faaf7409bf85f7b6509246e096980e8fe2
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-10-31 20:53:30 +0000

    Mon, Oct 31, 2022, 8:53 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -280,6 +280,8 @@ def sell():
             return apology("Could not find that symbol")
 
         print(shares)
+        for share in shares:
+            if symbol not in share
 
         db.execute("INSERT INTO transactions (user_id, symbol, shares, price, date, buy_sell) VALUES (?, ?, ?, ?, ?, ?)", session["user_id"], symbolInfo["symbol"], sharesToSell, symbolInfo["price"], date, "Sold")
         db.execute("UPDATE users SET cash = ? WHERE id = ?", newCashAmount, session["user_id"])
