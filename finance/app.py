ee2f4657e46ed29252da720e98ad48fc8baab03e
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-10-31 06:15:06 +0000

    Mon, Oct 31, 2022, 6:15 AM +00:00

--- finance/app.py
+++ finance/app.py
@@ -74,7 +74,7 @@ def buy():
         if not symbol or not shares:
             return apology("No fields can be blank")
 
-        if shares < 0 or shares.is_integer() == False:
+        if shares < 0 or shares % 1 == 0:
             return apology("Invalid Share Amount")
 
         if symbolInfo == None:
