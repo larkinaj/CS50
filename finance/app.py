cae94cba8023de129d08f44817b1d46a509528d0
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-10-31 20:35:44 +0000

    Mon, Oct 31, 2022, 8:35 PM +00:00

--- finance/app.py
+++ finance/app.py
@@ -265,12 +265,22 @@ def sell():
 
         symbol = request.form.get("symbol")
         sharesToSell = int(request.form.get("shares"))
+
+        if not symbol or not sharesToSell:
+            return apology("No fields can be blank")
+        if sharesToSell < 0:
+            return apology("Invalid Share Amount")
+        if symbolInfo == None:
+            return apology("Could not find that symbol")
+
         date = datetime.datetime.now()
         currentUser = db.execute("SELECT * FROM users WHERE id = ?", session["user_id"])[0]
         usersCash = currentUser["cash"]
         symbolInfo = lookup(symbol)
         transactionPrice = sharesToSell * symbolInfo["price"]
         newCashAmount = usersCash + transactionPrice
+
+
 
         db.execute("INSERT INTO transactions (user_id, symbol, shares, price, date, buy_sell) VALUES (?, ?, ?, ?, ?, ?)", session["user_id"], symbolInfo["symbol"], sharesToSell, symbolInfo["price"], date, "Sold")
         db.execute("UPDATE users SET cash = ? WHERE id = ?", newCashAmount, session["user_id"])
