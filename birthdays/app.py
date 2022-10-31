bbd9da069557fee66e330c2007f396bb2ca1fbc3
larkinaj <31088037+larkinaj@users.noreply.github.com>
2022-10-31 01:08:50 +0000

    Mon, Oct 31, 2022, 1:08 AM +00:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -27,8 +27,10 @@ def index():
     if request.method == "POST":
 
         # TODO: Add the user's entry into the database
-        name = 
-
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
+        db.execute("INSERT INTO birthdays (name, sport) VALUES(?, ?)", name, sport)
         return redirect("/")
 
     else:
